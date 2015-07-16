b1aa9be456416135f7586ea38c3416312432201c
Stephen Price <steeef@gmail.com>
2015-07-16 10:51:38 -0700

    Use "Extra" repo to install EPEL fix sshd_config lines don't change root pw

--- packer/scripts/ansible.sh
+++ packer/scripts/ansible.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -eux
 # Add the EPEL repository, and install Ansible.
-rpm -ivh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+yum -y install epel-release
 yum -y install ansible python-setuptools
