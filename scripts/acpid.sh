6c5c734b9e03916defbfbed0eb9c3ab2b676ebfb
Stephen Price <steeef@gmail.com>
2015-07-15 17:03:00 -0700

    remove unnecessary scripts

--- scripts/acpid.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# to enable ACPI event handling (useful for poweroff events)
-
-yum install -y acpid
-systemctl enable acpid
-systemctl start acpid
--- scripts/sshd.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sed -i 's/^.*UseDNS yes/UseDNS no/' /etc/ssh/sshd_config
