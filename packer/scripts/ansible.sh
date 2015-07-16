009d223174c009465a3c7dc2462c92dce0adb19e
Stephen Price <steeef@gmail.com>
2015-07-16 13:48:50 -0700

    allow proxies through

--- packer/scripts/ansible.sh
+++ packer/scripts/ansible.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -eux
-# Add the EPEL repository, and install Ansible.
+# debug yum http calls
+export URLGRABBER_DEBUG=1
+# Add the EPEL repository and install Ansible.
 yum -y install epel-release
 yum -y install ansible python-setuptools
